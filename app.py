7706a18103e1f0311f784496088de722a6b987f9
Jayaraj Shetty <lifjrs@gmail.com>
2025-07-24 23:06:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import json
+import os
 from datetime import datetime
 
 app = Flask(__name__)
@@ -34,4 +35,5 @@ def export_webhooks():
     return jsonify(webhook_storage)
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
