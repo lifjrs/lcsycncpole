13f88d25486ab1a987b2e0bd2883417732e0b1b3
Jayaraj Shetty <lifjrs@gmail.com>
2025-07-24 23:47:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,6 @@ def health_check():
     return "✅ Webhook server is running", 200
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
